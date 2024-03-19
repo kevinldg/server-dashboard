911f6fb48226b2971c4c1842acdc271e4fe7ef29
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-19 21:58:25 +0100

    Create a auth section and add to header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,10 @@
+import AuthSection from "../AuthSection/AuthSection";
+
 export default function Header() {
   return (
-    <header className=" bg-neutral-500 text-white p-4">
+    <header className=" bg-neutral-600 text-white shadow-md p-4 flex justify-between items-center">
       <h1 className="font-bold text-xl">Server Dashboard</h1>
+      <AuthSection />
     </header>
   );
 }
