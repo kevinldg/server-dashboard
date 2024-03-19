c9793ec9fe3a95a1ef9d3d4703eaae915f19ebfa
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-19 21:49:56 +0100

    Create a header and add to _app.js layout

---
+++ components/Header/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className=" bg-neutral-500 text-white p-4">
+      <h1 className="font-bold text-xl">Server Dashboard</h1>
+    </header>
+  );
+}
