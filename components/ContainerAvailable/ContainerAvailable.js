6c9a5e30f5575dc152d4cce1cb8e4bb67311da26
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-21 05:19:21 +0100

    Split up the dashboard into available and creatable containers

--- components/ContainerAvailable/ContainerAvailable.js
+++ components/ContainerAvailable/ContainerAvailable.js
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDocker } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
-export default function DockerContainer({ name, isOnline }) {
+export default function ContainerAvailable({ name, isOnline }) {
   return (
-    <div className=" bg-neutral-200 w-80 shadow hover:scale-[1.025] transition-transform">
+    <Link
+      href={"/container/" + name}
+      className=" bg-neutral-200 w-80 shadow hover:scale-[1.025] transition-transform"
+    >
       <div className="px-4 py-2 flex justify-between items-center">
         <div className="flex items-center gap-2">
           <FontAwesomeIcon icon={faDocker} />
@@ -15,6 +19,6 @@ export default function DockerContainer({ name, isOnline }) {
           <div className="bg-red-500 w-3 h-3 rounded-full"></div>
         )}
       </div>
-    </div>
+    </Link>
   );
 }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,40 @@
-import DockerContainer from "@/components/DockerContainer/DockerContainer";
+import ContainerAvailable from "@/components/ContainerAvailable/ContainerAvailable";
+import ContainerCreate from "@/components/ContainerCreate/ContainerCreate";
 import ServerStatus from "@/components/ServerStatus/ServerStatus";
 
-export default function Dashboard() {
-  const username = "GuNShOtzZ";
+const username = "GuNShOtzZ";
 
+const availableContainers = [
+  {
+    name: "minecraft-server-01",
+    isOnline: true,
+  },
+  {
+    name: "garrysmod-server-01",
+    isOnline: false,
+  },
+  {
+    name: "counterstrike-server-01",
+    isOnline: true,
+  },
+];
+
+const containersToCreate = [
+  {
+    name: "Minecraft Vanilla",
+  },
+  {
+    name: "Minecraft Forge",
+  },
+  {
+    name: "Teamspeak 3",
+  },
+  {
+    name: "Counter Strike 2",
+  },
+];
+
+export default function Dashboard() {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
@@ -13,9 +44,17 @@ export default function Dashboard() {
       <div className="flex flex-col gap-4">
         <p className="font-bold">Folgende Container sind verfügbar:</p>
         <div className="flex flex-col gap-2">
-          <DockerContainer name="Minecraft Vanilla" isOnline />
-          <DockerContainer name="Garry's Mod" />
-          <DockerContainer name="Counter Strike 2 Server" isOnline />
+          {availableContainers.map(({ name, isOnline }) => (
+            <ContainerAvailable name={name} isOnline={isOnline} />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p className="font-bold">Container erstellen</p>
+        <div className="flex flex-col gap-2">
+          {containersToCreate.map(({ name }) => (
+            <ContainerCreate name={name} />
+          ))}
         </div>
       </div>
     </div>
