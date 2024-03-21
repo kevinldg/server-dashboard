efd943478525b1ef235cefd2dc1add6f8bb3c1d1
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-21 05:42:58 +0100

    Adjust styling of _app.js layout

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="p-4">
+      <main className="p-4 flex flex-col gap-4">
         <Component {...pageProps} />
       </main>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  );
+  return <></>;
 }
