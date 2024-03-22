c0da31085a693030a405656187cc0ae44b72e52b
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-22 08:16:34 +0100

    Change document title

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Next.js App</title>
+        <title>Server Dashboard</title>
       </Head>
       <body>
         <Main />
