ea14ea860c842f6c8eb66c593f4296dc89db1170
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-22 02:26:14 +0100

    Deactivate AuthSection in header until real implementation

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className=" bg-neutral-600 text-white shadow-md p-4 flex justify-between items-center">
       <h1 className="font-bold text-xl">Server Dashboard</h1>
-      <AuthSection />
+      {/* <AuthSection /> */}
     </header>
   );
 }
