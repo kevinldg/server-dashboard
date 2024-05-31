720c351d7eaba293f21940cd65262beb9d3e7db4
GuNShOtzZ <kevinloeding@outlook.de>
2024-05-31 22:24:39 +0200

    Rework: Adjust page layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import Header from "@/components/Header/Header";
+import Link from "next/link";
 import "@/styles/globals.css";
+
+import Header from "@/components/Header/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="p-4 flex flex-col gap-4">
+      <main className="p-4">
         <Component {...pageProps} />
       </main>
     </>
